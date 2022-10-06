6c649ddb8993227763da21cf2a867084efa23b37
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-07 03:21:37 +0600

    Были созданы модели задач, статуса и типа Сделал миграцию и добавил их в админку Сделал дамп бд

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from tracker.models import IssueTracker, Type, Status
+
 # Register your models here.
+admin.site.register(IssueTracker)
+admin.site.register(Type)
+admin.site.register(Status)
+
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class IssueTracker(models.Model):
+    summary = models.CharField(verbose_name='Заголовок', max_length=200, null=False, blank=False)
+    description = models.TextField(verbose_name='Подробное описание', max_length=500, null=True, blank=True)
+    status = models.ManyToManyField('tracker.Status', related_name='statuses', blank=True)
+    type = models.ManyToManyField('tracker.Type', related_name='types', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    changed_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.summary}'
+
+
+class Type(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Status(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
